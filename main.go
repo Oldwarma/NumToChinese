d61c75520b2af031760ee96d95a6d6fd70508fa7
ajie <1336993605@qq.com>
2023-08-27 23:40:57 +0800

    中文数字与Go语言数字的相互转换。

---
+++ main.go
@@ -0,0 +1,17 @@
+package NumToChinese
+
+import "fmt"
+
+func main() {
+	chNum := "负十七亿零五十三万七千零一十六"
+	num, _ := DecodeToInt64(chNum)
+	fmt.Println(num) // -1700537016
+	chNumAgain := EncodeFromInt64(num)
+	fmt.Println(chNumAgain) // 负十七亿零五十三万七千零一十六
+
+	chFloatNum := "负零点零七三零六"
+	fNum, _ := DecodeToFloat64(chFloatNum)
+	fmt.Printf("%f\n", fNum) // -0.073060
+	chFloatNumAgain := EncodeFromFloat64(fNum)
+	fmt.Println(chFloatNumAgain) // 负零点零七三零六
+}
